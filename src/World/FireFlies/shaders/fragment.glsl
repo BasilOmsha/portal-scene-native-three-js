1251293981dabf19a086d3c313cb0839d02815bc
Basel <107993017+BasilOmsha@users.noreply.github.com>
2024-10-11 15:17:34 +0300

    calculating the center of the uv coords and showing it in the center by adding it to the alpha and activatinf trancperancy

--- src/World/FireFlies/shaders/fragment.glsl
+++ src/World/FireFlies/shaders/fragment.glsl
@@ -2,8 +2,9 @@
 
 void main() {
 
+    float distanceToCenter = distance(gl_PointCoord, vec2(0.5));
 
-    gl_FragColor = vec4(gl_PointCoord, 1.0, 1.0);
+    gl_FragColor = vec4(1.0, 1.0, 1.0, distanceToCenter);
 
-    
+
 }
